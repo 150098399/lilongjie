121aa2ae3e646eb7ff72c204928306ebfc6763d8
lilongjie <18373146601@163.com>
2023-07-22 23:46:59 +0800

    新建项目

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  base: '/lilongjie.github.io',
+  base: '/lilongjie',
   lang: 'zh-CN',
   title: '三味书屋',
   description: '踩坑日记&&生活记录'
